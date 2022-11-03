8414f99dede71b1c23eb6f891345be595e1ac926
SaraCamara <sarapcamara@gmail.com>
2022-11-03 13:46:26 +0000

    Apache v1.0

---
+++ script-iac2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Atualizando servidor"
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando apache, unzip e wget"
+apt-get install apache2 -y
+apt-get install unzip -y
+apt-get install wget -y
+
+echo "Baixando e copiando os arquivos da aplicacao"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "fim"
+
